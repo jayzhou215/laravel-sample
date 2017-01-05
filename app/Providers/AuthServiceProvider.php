ca5d2ce0d6ced1a410d47ae5c8ad5e7b10f83cfd
jay <jayzhou215@163.com>
2017-01-05 09:05:20 +0000

    add user statuses

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 use App\Models\User;
 use App\Policies\UserPolicy;
 
+use App\Models\Status;
+use App\Policies\StatusPolicy;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -17,7 +20,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
-        User::class => UserPolicy::class
+        User::class => UserPolicy::class,
+        Status::class => StatusPolicy::class,
     ];
 
     /**
